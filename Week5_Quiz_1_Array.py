6ff885a865bd78ef8006dc7657a06340de280e25
fionavaleri <70874630+fionavaleri@users.noreply.github.com>
2020-09-08 09:25:03 +1000

    Create Week5_Quiz_1_Array.py
    Arrays Week 5 Quiz

---
+++ Week5_Quiz_1_Array.py
@@ -0,0 +1,9 @@
+# Given the following python code:
+x = ()
+list = [66, 43, 1, 6, 2, 99, 4]
+
+# Output each number on a separate line if it is less than the number 10.
+
+for x in list:
+    if x <10:
+        print(x,'\n')
